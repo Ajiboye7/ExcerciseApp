6c4ae8859a1bc4abe7a997bd2d05e004e882212f
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-06-01 15:49:52 +0100

    latest

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,4 @@
-/*import React from 'react';
-import { NavLink } from 'react-router-dom';
-import logo from '../Images/logo.jpg';
-import '../style/Navbar.css'; 
-
-function Navbar() {
-  return (
-    <div className="navbar">
-      <img src={logo} alt="Logo" className="logo" />
-      <ul className="nav-links">
-        <li>
-          <NavLink to="/" exact activeClassName="active">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/blog" activeClassName="active">Blog</NavLink>
-        </li>
-        <li>
-          <NavLink to="/practical" activeClassName="active">Exercise</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" activeClassName="active">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register" activeClassName="active">Register</NavLink>
-        </li>
-      </ul>
-    </div>
-  );
-}
-
-export default Navbar;
-*/import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../Images/logo.jpg';
 

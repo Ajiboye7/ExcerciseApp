34b91e8964e147aa9ce567841681878e0895b7a3
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-28 19:34:25 +0100

    fetch video Api

--- src/components/ExcerciseVideos.js
+++ src/components/ExcerciseVideos.js
@@ -1,23 +1,4 @@
-import React from 'react';
-import '../style/ExcerciseVideos.css';
-
-const ExerciseVideos = ({ name, videos }) => {
-  return (
-    <div className="videos-container">
-      <h3>Related Videos</h3>
-      {videos.length ? (
-        <div className='video-header'>
-          <h4>Watch <span>{name}</span> Exercise Videos</h4>
-          <div className="videos-grid">
-            {videos.slice(0, 6).map((video, index) => (
-              <div key={index} className="video-card">
-                <a href={`https://www.youtube.com/watch?v=${video.video.videoId}`} target="_blank" rel="noopener noreferrer">
-                  <img src={video.video.thumbnails[0].url} alt={video.video.title} />
-                </a>
-                <h3>{video.video.title}</h3>
-                <p>{video.video.channelName}</p>
-              </div>
-            ))}
+import 
           </div>
         </div>
       ) : (

2290c79c23adbd9ea3269059ef5010688f06a86f
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-09-11 19:10:34 +0100

    axios refix

--- src/components/ExcerciseVideos.js
+++ src/components/ExcerciseVideos.js
@@ -10,11 +10,7 @@ const ExerciseVideos = ({ name, videos }) => {
           <h4>Watch <span>{name}</span> Exercise Videos</h4>
           <div className="videos-grid">
             {videos.slice(0, 6).map((video, index) => (
-              <div key={index} className="video-card">
-                <a href={`https://www.youtube.com/watch?v=${video.video.videoId}`} target="_blank" rel="noopener noreferrer">
-                  <img src={video.video.thumbnails[0].url} alt={video.video.title} />
-                </a>
-                <h3>{video.video.title}</h3>
+              <div key={index} className=" 
                 <p>{video.video.channelName}</p>
               </div>
             ))}

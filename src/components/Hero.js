cf38f60dfe178cc21b585e05a4b4060e9f654b22
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-05-12 19:45:02 +0100

    excercise

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,7 @@ function Hero() {
         <br/>Whether it's a daily jog, a gym workout, or a yoga session, staying active can improve your mood,boost your energy levels, and reduce the risk of chronic diseases.
         <br/>Start your fitness journey today and experience the benefits of a healthy lifestyle!</p>
         <p>Check out for the most effective excercise for you</p>
-        <button>Explore Excercise!</button>
+        <button>Explore Excerycise!</button>
       </div>
       <div className="hero-image">
         <img src={logo} alt="Exercise"/>

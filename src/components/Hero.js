e40d7bdc700a53d5a4921f00cbdf2a5d141a8ae8
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-06-08 20:04:27 +0100

    fix typographic error

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,8 +10,8 @@ function Hero() {
         <p>Engaging in regular physical activity is crucial for maintaining good health and wellness. 
         <br/>Whether it's a daily jog, a gym workout, or a yoga session, staying active can improve your mood,boost your energy levels, and reduce the risk of chronic diseases.
         <br/>Start your fitness journey today and experience the benefits of a healthy lifestyle!</p>
-        <p>Check out for the most effective excercises for you</p>
-        <button>Explore Excercise!</button>
+        <p>Check out for the most effective exercise for you</p>
+        <button>Explore Exercise</button>
       </div>
       <div className="hero-image">
         <img src={logo} alt="Exercise"/>

a5987956ab6e9592768737b9b535fc4c303526ea
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-09-11 19:47:17 +0100

    update

--- src/components/ExcerciseVideos.js
+++ src/components/ExcerciseVideos.js
@@ -21,7 +21,7 @@ const ExerciseVideos = ({ name, videos }) => {
           </div>
         </div>
       ) : (
-        <p>No videos found here. Try reloading the page.</p>
+        <p>No videos found here. Try reloading the page now</p>
       )}
     </div>
   );
